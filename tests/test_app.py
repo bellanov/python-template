a295dafeaa1632c1aba95dcbc7e045fd63a8a3f0
Bellanov Apilli <bellanovapilli@gmail.com>
2024-11-06 13:53:15 -0700

    Updated test script comments

--- tests/test_app.py
+++ tests/test_app.py
@@ -7,5 +7,5 @@ from python_template import app
 
 @pytest.mark.unit
 def test_app():
-    """Validate package is importable"""
+    """Validate package is importable."""
     assert app.app() == "Hello World!"
