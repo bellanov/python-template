61d6f59541540bf3b255183476e94b5a63e5dfb6
Bellanov <bellanovapilli@gmail.com>
2026-03-28 18:29:15 -0400

    Marked Unit Tests

--- tests/test_hello_world.py
+++ tests/test_hello_world.py
@@ -4,6 +4,7 @@ import pytest
 from samples.hello_world.app import hello_world
 
 
+@pytest.mark.unit  # type: ignore
 def test_hello_world():
     """Test the hello_world function."""
     assert hello_world() == "Hello, World!"
