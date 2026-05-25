356a9077454647d3b8693d4e54b6c2efb97591f2
Bellanov <bellanovapilli@gmail.com>
2026-05-25 00:07:42 -0400

    Updated Unit Tests

--- tests/dsa/test_retry_pattern.py
+++ tests/dsa/test_retry_pattern.py
@@ -2,10 +2,10 @@
 
 import pytest
 
-from samples.dsa.retry_pattern import retry
+from samples.dsa.retry_pattern import retry_function
 
 
 @pytest.mark.unit  # type: ignore
 def test_retry():
     """Test the retry function."""
-    assert retry.retry() == True
+    assert retry_function.fetch_joke() is not None
