d9b1edbb3f4fa819b8c07ff2db105a31740ecbdc
Bellanov <bellanovapilli@gmail.com>
2026-05-30 16:47:21 -0400

    Added registry Sample

---
+++ samples/dsa/registry_pattern/registry.py
@@ -0,0 +1,37 @@
+class PluginRegistry:
+    _registry = {}
+
+    @classmethod
+    def register(cls, name):
+        """Decorator to register a plugin by name."""
+        def decorator(plugin_cls):
+            cls._registry[name] = plugin_cls
+            return plugin_cls
+        return decorator
+
+    @classmethod
+    def get(cls, name):
+        if name not in cls._registry:
+            raise KeyError(f"Plugin '{name}' not found. Available: {list(cls._registry)}")
+        return cls._registry[name]
+
+
+# --- Registering implementations ---
+
+@PluginRegistry.register("csv")
+class CsvExporter:
+    def export(self, data):
+        return f"Exporting {data} as CSV"
+
+@PluginRegistry.register("json")
+class JsonExporter:
+    def export(self, data):
+        return f"Exporting {data} as JSON"
+
+
+# --- Using the registry ---
+
+format_type = "json"
+exporter = PluginRegistry.get(format_type)()
+print(exporter.export({"key": "value"}))
+# Output: Exporting {'key': 'value'} as JSON
